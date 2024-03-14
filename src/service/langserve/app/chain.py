a6e2421ac204326616bc487ac2fe238d31121ada
woonsango <doldol9080@kookmin.ac.kr>
2024-03-14 20:26:06 +0900

    [Feat] 여러 타입의 AI를 사용할 수 있도록 설정 #19

--- src/service/langserve/app/chain.py
+++ src/service/langserve/app/chain.py
@@ -1,5 +1,5 @@
 import os
-import json
+import yaml
 from dotenv import load_dotenv
 # langchain 관련 라이브러리들을 가져옵니다.
 from langchain.chains import LLMChain
@@ -17,18 +17,25 @@ from langchain.prompts import (
 # .env 파일에서 환경변수를 로드합니다.
 load_dotenv()
 
-# JSON 파일로부터 설정을 로드하는 함수를 정의합니다.
-def load_settings_from_json(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
+def load_yaml(file_name):
+    with open(file_name) as file:
+        settings = yaml.safe_load(file)
+    return settings
 
-# 설정 파일을 로드합니다. 파일 경로는 필요에 따라 조정하세요.
-settings = load_settings_from_json('template/1.json')
+class Settings:
 
-# 시스템 메시지 템플릿을 생성합니다. {Q_list} 부분은 질문 리스트로 대체됩니다.
-system_template = SystemMessagePromptTemplate.from_template(
-    settings["system_template_content"].format(Q_list=settings["question_list"])
-)
+    def __init__(self, type):
+        settings = load_yaml('template/template.yml')
+
+        # 시스템 메시지 템플릿을 생성합니다. {Q_list} 부분은 질문 리스트로 대체됩니다.
+        self.system_template = SystemMessagePromptTemplate.from_template(
+            settings[type]["system_template_content"].format(Q_list=settings[type]["question_list"])
+        )
+
+        self.ai_prefix = settings[type]['ai_prefix']
+        self.human_prefix = settings[type]['human_prefix']
+        
+settings = Settings('cool_headed')
 
 # 벡터 스토어를 생성합니다. 여기서는 텍스트 파일로부터 FAISS 인덱스를 생성합니다.
 vector_store = FAISS.from_texts(
@@ -44,7 +51,7 @@ llm = ChatOpenAI(model_name='gpt-3.5-turbo')
 
 # 대화 프롬프트를 생성합니다. 이 프롬프트는 대화의 구조를 정의합니다.
 prompt = ChatPromptTemplate.from_messages([
-    system_template,                                                # 역할 부여
+    settings.system_template,                                                # 역할 부여
     MessagesPlaceholder(variable_name="chat_history"),              # 대화 내역을 메모리 저장소에 저장
     HumanMessagePromptTemplate.from_template("{human_input}"),      # 사용자 입력을 템플릿에 삽입
 ])
@@ -52,8 +59,8 @@ prompt = ChatPromptTemplate.from_messages([
 # 대화 메모리를 생성합니다. 이 메모리는 대화 내역을 저장하고 관리합니다.
 memory = ConversationBufferMemory(
     memory_key="chat_history",                                      # 대화 내역의 키
-    ai_prefix=settings["ai_prefix"],                                # AI 메시지 접두사
-    human_prefix=settings["human_prefix"],                          # 사용자 메시지 접두사
+    ai_prefix = settings.ai_prefix,                                # AI 메시지 접두사
+    human_prefix = settings.human_prefix,                          # 사용자 메시지 접두사
     return_messages=True                                            # 메시지 반환 여부
 )
 
