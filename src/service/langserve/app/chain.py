ae0b082dbaaadc027bab9ff2f1c235d112fc6ecc
hyunjoon0208 <juatis96@kookmin.ac.kr>
2024-03-15 18:12:04 +0900

    Feat/langserve-API-추가-구현-#23

--- src/service/langserve/app/chain.py
+++ src/service/langserve/app/chain.py
@@ -8,66 +8,69 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import (
-    ChatPromptTemplate,
-    MessagesPlaceholder,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate
+	ChatPromptTemplate,
+	MessagesPlaceholder,
+	SystemMessagePromptTemplate,
+	HumanMessagePromptTemplate
 )
 
 # .env 파일에서 환경변수를 로드합니다.
 load_dotenv()
 
 def load_yaml(file_name):
-    with open(file_name) as file:
-        settings = yaml.safe_load(file)
-    return settings
+	with open(file_name) as file:
+		settings = yaml.safe_load(file)
+	return settings
 
 class Settings:
+	
+	def __init__(self, type):
+		settings = load_yaml('template/template.yml')
 
-    def __init__(self, type):
-        settings = load_yaml('template/template.yml')
+		# 시스템 메시지 템플릿을 생성합니다. {Q_list} 부분은 질문 리스트로 대체됩니다.
+		self.system_template = SystemMessagePromptTemplate.from_template(
+			settings[type]["system_template_content"].format(Q_list=settings[type]["question_list"])
+		)
 
-        # 시스템 메시지 템플릿을 생성합니다. {Q_list} 부분은 질문 리스트로 대체됩니다.
-        self.system_template = SystemMessagePromptTemplate.from_template(
-            settings[type]["system_template_content"].format(Q_list=settings[type]["question_list"])
-        )
+		self.ai_prefix = settings[type]['ai_prefix']
+		self.human_prefix = settings[type]['human_prefix']
 
-        self.ai_prefix = settings[type]['ai_prefix']
-        self.human_prefix = settings[type]['human_prefix']
-        
-settings = Settings('cool_headed')
+	def load_yaml(file_name):
+		with open(file_name) as file:
+			settings = yaml.safe_load(file)
+		return settings
 
-# 벡터 스토어를 생성합니다. 여기서는 텍스트 파일로부터 FAISS 인덱스를 생성합니다.
-vector_store = FAISS.from_texts(
-    '../vector_store/a.txt',
-    embedding=OpenAIEmbeddings(),
-)
 
-# 검색기(retriever)를 벡터 스토어로부터 생성합니다.
-retriever = vector_store.as_retriever()
+class ChainV1: #phi 2
+	def __init__(self, history_id, template):
+		pass
 
-# OpenAI의 Chat 모델을 생성합니다. 모델명은 설정에 따라 달라질 수 있습니다.
-llm = ChatOpenAI(model_name='gpt-3.5-turbo')
 
-# 대화 프롬프트를 생성합니다. 이 프롬프트는 대화의 구조를 정의합니다.
-prompt = ChatPromptTemplate.from_messages([
-    settings.system_template,                                                # 역할 부여
-    MessagesPlaceholder(variable_name="chat_history"),              # 대화 내역을 메모리 저장소에 저장
-    HumanMessagePromptTemplate.from_template("{human_input}"),      # 사용자 입력을 템플릿에 삽입
-])
+class ChainV2: #gpt 3.5
+	def __init__(self, history_id, template):
+		self.history_id = history_id
+		self.template = template
+		self.llm = ChatOpenAI(model_name='gpt-3.5-turbo')
+		self.settings = Settings(template)
+		self.prompt = ChatPromptTemplate.from_messages([
+				self.settings.system_template,                                                # 역할 부여
+				MessagesPlaceholder(variable_name=history_id),              # 대화 내역을 메모리 저장소에 저장
+				HumanMessagePromptTemplate.from_template("{human_input}"),      # 사용자 입력을 템플릿에 삽입
+			])
+		self.memory = ConversationBufferMemory(
+					memory_key=history_id,                                      # 대화 내역의 키
+					ai_prefix = self.settings.ai_prefix,                                # AI 메시지 접두사
+					human_prefix = self.settings.human_prefix,                          # 사용자 메시지 접두사
+					return_messages=True                                            # 메시지 반환 여부
+				)
+		self.model = LLMChain(
+			llm=self.llm,
+			prompt=self.prompt,
+			memory=self.memory,
+			verbose=True,                                                   # 자세한 로그 출력 여부
+		)
 
-# 대화 메모리를 생성합니다. 이 메모리는 대화 내역을 저장하고 관리합니다.
-memory = ConversationBufferMemory(
-    memory_key="chat_history",                                      # 대화 내역의 키
-    ai_prefix = settings.ai_prefix,                                # AI 메시지 접두사
-    human_prefix = settings.human_prefix,                          # 사용자 메시지 접두사
-    return_messages=True                                            # 메시지 반환 여부
-)
 
-# LLM 체인을 생성합니다. 이 체인은 대화 프롬프트, 메모리, 모델 등을 연결하여 대화를 처리합니다.
-Chain = LLMChain(
-    llm=llm,
-    prompt=prompt,
-    memory=memory,
-    verbose=True,                                                   # 자세한 로그 출력 여부
-)
+class ChainV3: #gpt 2
+	def __init__(self) -> None:
+		pass
--- src/service/langserve/app/server.py
+++ src/service/langserve/app/server.py
@@ -3,24 +3,88 @@ from fastapi.responses	import RedirectResponse
 from pydantic 			import BaseModel, Field
 from typing				import List, Union
 from langchain.schema	import AIMessage, HumanMessage, SystemMessage
-from app.chain			import Chain
-
+from app.chain			import ChainV1, ChainV2, ChainV3
+from bson import ObjectId
 app = FastAPI(title = "LLMChain", description = "Large Language Model Chain", version = "0.1.0")
 
+models_dict = {}
+
+# Pydantic 모델에서 ObjectId를 사용하기 위한 클래스
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid ObjectId')
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+
+class ConnectRequest(BaseModel):
+    #history_id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+	history_id: str
 class ChatRequest(BaseModel):
-	content: str
+    #history_id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    history_id: str
+    content: str
 
 class ChatResponse(BaseModel):
 	content: str
 
-# 자체 제작하는 LLM을 v1으로, GPT-3.5-turbo를 v2로 설정하면 어떨까?
-@app.post("/v1/chat", response_model=ChatResponse)
-async def chat(request: ChatRequest):
-	return ChatResponse(content=Chain(request.content)['text'])
+@app.post("/v1/chat/connect", response_model=ChatResponse)
+async def connect_v1(request : ConnectRequest):
+	chain = ChainV1(history_id = request.history_id, template = "cool_headed")
+	models_dict[request.history_id]= chain
+	return ChatResponse(content="Connected to v1")
 
-@app.post("/v2/chat", response_model=ChatResponse)
-async def chat(request: ChatRequest):
-	return ChatResponse(content=Chain(request.content)['text'])
+@app.post("/v2/chat/connect", response_model=ChatResponse)
+async def connect_v2(request : ConnectRequest):
+	chain = ChainV2(history_id = request.history_id, template = "cool_headed")
+	models_dict[request.history_id]= chain
+	return ChatResponse(content="Connected to v2")
+
+@app.post("/v3/chat/connect", response_model=ChatResponse)
+async def connect_v3(request : ConnectRequest):
+	chain = ChainV3(history_id = request.history_id, template = "cool_headed")
+	models_dict[request.history_id]= chain
+	return ChatResponse(content="Connected to v3")
+
+# 이렇게 하면 v1, v2, v3 모두 같은 endpoint를 사용할 수 있게 됩니다.
+@app.post("/v1/chat/disconnect", response_model=ChatResponse)
+async def disconnect_v1(request : ConnectRequest):
+	models_dict.pop(request.history_id)
+	return ChatResponse(content="Disconnected from v1")
+
+@app.post("/v2/chat/disconnect", response_model=ChatResponse)
+async def disconnect_v2(request : ConnectRequest):
+	models_dict.pop(request.history_id)
+	return ChatResponse(content="Disconnected from v2")
+
+@app.post("/v3/chat/disconnect", response_model=ChatResponse)
+async def disconnect_v3(request : ConnectRequest):
+	models_dict.pop(request.history_id)
+	return ChatResponse(content="Disconnected from v3")
+
+
+# 자체 제작하는 LLM을 v1으로, GPT-3.5-turbo를 v2로 설정하면 어떨까?
+@app.post("/v1/chat/invoke", response_model=ChatResponse)
+async def chatV1(request: ChatRequest):
+	return ChatResponse(content=models_dict[request.history_id](request.content))
+
+@app.post("/v2/chat/invoke", response_model=ChatResponse)
+async def chatV2(request: ChatRequest):
+	return ChatResponse(content=models_dict[request.history_id].model(request.content)['text'])
+
+@app.post("/v3/chat/invoke", response_model=ChatResponse)
+async def chatV2(request: ChatRequest):
+	return ChatResponse(content=models_dict[request.history_id](request.content))
+
+
 
 if __name__ == "__main__":
 	import uvicorn
