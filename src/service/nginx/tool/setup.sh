17642dad580205a61a245da2573a94116d9d3465
younganswer <55086725+younganswer@users.noreply.github.com>
2024-03-16 22:00:40 +0900

    Update setup.sh

--- src/service/nginx/tool/setup.sh
+++ src/service/nginx/tool/setup.sh
@@ -7,22 +7,16 @@ set -e
 if [ "$LOCAL" == 'true' ]; then
 	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $NEXTJS_CERTS_KEY -out $NEXTJS_CERTS -subj "/C=MO/L=KH/O=1337/OU=student/CN=$DOMAIN_NAME"
 	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $NESTJS_CERTS_KEY -out $NESTJS_CERTS -subj "/C=MO/L=KH/O=1337/OU=student/CN=dev.$DOMAIN_NAME"
-
-	sed -i "s|nextjs_certs_key|$NEXTJS_CERTS_KEY|g"	"/etc/nginx/conf.d/default.conf"
-	sed -i "s|nextjs_certs|$NEXTJS_CERTS|g"			"/etc/nginx/conf.d/default.conf"
-	sed -i "s|nestjs_certs_key|$NESTJS_CERTS_KEY|g"	"/etc/nginx/conf.d/default.conf"
-	sed -i "s|nestjs_certs|$NESTJS_CERTS|g"			"/etc/nginx/conf.d/default.conf"
-else
-	sed -i "s|443 ssl|443|g"		"/etc/nginx/conf.d/default.conf"
-	sed -i "/ssl_certificate/d"		"/etc/nginx/conf.d/default.conf"
-	sed -i "/ssl_certificate_key/d"	"/etc/nginx/conf.d/default.conf"
-	sed -i "/ssl_protocols/d"		"/etc/nginx/conf.d/default.conf"
 fi
 
 # Server config
+sed -i "s|my_domain|$DOMAIN_NAME|g"				"/etc/nginx/conf.d/default.conf"
 sed -i "s|nextjs_port|$NEXTJS_PORT|g"			"/etc/nginx/conf.d/default.conf"
 sed -i "s|nestjs_port|$NESTJS_PORT|g"			"/etc/nginx/conf.d/default.conf"
-sed -i "s|my_domain|$DOMAIN_NAME|g"	"/etc/nginx/conf.d/default.conf"
+sed -i "s|nextjs_certs_key|$NEXTJS_CERTS_KEY|g"	"/etc/nginx/conf.d/default.conf"
+sed -i "s|nestjs_certs_key|$NESTJS_CERTS_KEY|g"	"/etc/nginx/conf.d/default.conf"
+sed -i "s|nextjs_certs|$NEXTJS_CERTS|g"			"/etc/nginx/conf.d/default.conf"
+sed -i "s|nestjs_certs|$NESTJS_CERTS|g"			"/etc/nginx/conf.d/default.conf"
 
 # Log config
 sed -i '40s/.*/\tlog_format debug '\''$remote_addr - $remote_user [$time_local] '\'' '\''"$request" $status $body_bytes_sent '\'' '\''"$http_referer" "$http_user_agent"'\'';/' /etc/nginx/nginx.conf
