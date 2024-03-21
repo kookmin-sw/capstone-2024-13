9926ad2b6ee0d2cf9ebcc81aef93e301ff6cdf47
woonsango <doldol9080@kookmin.ac.kr>
2024-03-22 04:09:04 +0900

    [Feat] STT API 구현 #39

---
+++ src/service/langserve/app/stt/___init__.py
@@ -0,0 +1,16 @@
+import openai
+from pydub import AudioSegment
+
+class stt:
+    def __init__(self) :
+        self.client = openai.OpenAI()
+    def execution(self, audio_file_name):
+        audio_file = open('audio_file_name', 'rb')
+
+        self.transcript = self.client.audio.transcriptions.create(
+                        model='whisper-1',
+                        file=audio_file,
+                        response_format='text'
+                    )
+        
+        return self.transcript
