eb627c1cce9ca4bb97547e4fda183d90d9c19a1e
yoon2000 <55117130+yoon2000@users.noreply.github.com>
2024-03-25 17:38:01 +0900

    [Fix] User, Diary Schema 구조 설계 및 수정 #18

--- src/service/nestjs/src/api/diary/diary.service.ts
+++ src/service/nestjs/src/api/diary/diary.service.ts
@@ -1,14 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Diary } from 'src/common/database/schema';
+import { Types } from 'mongoose';
 
 @Injectable()
 class DiaryService {
 	constructor(@InjectModel('Diary') private readonly diaryModel) {}
 
-	async get(_id: string): Promise<Diary> {
+	async get(diaryId: string) {
 		try {
-			return await this.diaryModel.findOne({ _id });
+			return await this.diaryModel.findById(diaryId);
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async getUser(userId: Types.ObjectId) {
+		try {
+			return await this.diaryModel.find({ userId: userId });
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async create(userId: Types.ObjectId, title: string, content: string) {
+		try {
+			return await new this.diaryModel({ userId, title, content }).save();
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async update(diaryId: string, title: string, content: string) {
+		try {
+			return await this.diaryModel.findByIdAndUpdate(diaryId, { title, content }, { new: true });
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async delete(diaryId: string) {
+		try {
+			return await this.diaryModel.findByIdAndDelete(diaryId);
 		} catch (error) {
 			throw error;
 		}
