62075bc24d484f0c500c3850840cc69a7615aa2d
hyunjoon0208 <juatis96@kookmin.ac.kr>
2024-03-26 10:36:18 +0900

    tts/__init__.py에서 file system에 저장 및 삭제하는 부분을 temp file로 변경

--- src/service/mystic/app/tts/__init__.py
+++ src/service/mystic/app/tts/__init__.py
@@ -1,13 +1,10 @@
 import os
 from gtts import gTTS
 from io import BytesIO
-from glob import glob
+import tempfile
 from pydub import AudioSegment
 
-def clean_cache():
-        for file in glob("./ffcache*"):
-            os.remove(file)
-def init(text, slow = False):
+def init(text, slow=False):
     mp3_fp = BytesIO()
     gtts = gTTS(text=text, lang="ko", slow=slow)
     gtts.write_to_fp(mp3_fp)
@@ -15,11 +12,12 @@ def init(text, slow = False):
     return mp3_fp
 
 def forward(mp3_fp):
-	audio = AudioSegment.from_file(mp3_fp, format="mp3")
-	for file in glob("./ffcache*"):
-		os.remove(file)
-	return audio.export(format="mp3").read()
+    audio = AudioSegment.from_file(mp3_fp, format="mp3")
+    with tempfile.TemporaryFile() as temp_file:
+        audio.export(temp_file, format="mp3")
+        temp_file.seek(0)
+        return temp_file.read()
 
 def tts(text, slow):
-	mp3_fp = init(text, slow)
-	return forward(mp3_fp)
+    mp3_fp = init(text, slow)
+    return forward(mp3_fp)
