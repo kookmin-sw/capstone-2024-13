1faf01681a86e652b4825626d101c047a6ab7ba5
yoon2000 <55117130+yoon2000@users.noreply.github.com>
2024-05-02 13:41:18 +0900

    fix/logout-redirection

--- src/service/nextjs/src/container/(private)/mypage/logout-button/index.tsx
+++ src/service/nextjs/src/container/(private)/mypage/logout-button/index.tsx
@@ -9,14 +9,10 @@ const MyPageLogoutButton = () => {
 	const router = useRouter();
 
 	const handleClick = async () =>
-		await postLogout()
-			.then(() => {
-				router.push('/auth/login');
-			})
-			.catch((error: Error) => {
-				console.error(error);
-				alert('로그아웃에 실패했습니다. 다시 시도해주세요.');
-			});
+		await postLogout().catch((error: Error) => {
+			console.error(error);
+			alert('로그아웃에 실패했습니다. 다시 시도해주세요.');
+		});
 
 	return (
 		<div className={style.container}>
