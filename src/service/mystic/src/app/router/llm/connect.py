eaa86bacdd0ad97bd8d3280990634c137f5435a7
younganswer <55086725+younganswer@users.noreply.github.com>
2024-05-11 00:25:00 +0900

    Update connect.py

--- src/service/mystic/src/app/router/llm/connect.py
+++ src/service/mystic/src/app/router/llm/connect.py
@@ -2,26 +2,27 @@ import os
 
 from fastapi	import APIRouter, HTTPException
 from pydantic 	import BaseModel
-from bson		import ObjectId
-from chain		import ChainV1, ChainV2, ChainV3
+from bson	import ObjectId
+from chain	import ChainV1, ChainV2, ChainV3
 from app.util	import redis_instance
 from app.util	import connection
 
 router = APIRouter(prefix="/connect", tags=["connect"])
 
 class ConnectRequest(BaseModel):
+	version: str
 	template_id: int
 
 class ConnectResponse(BaseModel):
 	connection_id: str
 
-@router.post("/{version}", response_model=ConnectResponse)
-async def connect(version: str, request: ConnectRequest):
-	if version not in ["v1", "v2", "v3"]:
+@router.post("/", response_model=ConnectResponse)
+async def connect(request: ConnectRequest):
+	if request.version not in ["v1", "v2", "v3"]:
 		raise HTTPException(status_code=400, detail="Bad Request")
 	connection_id = str(ObjectId())
 	connection[connection_id] = {
-		version: version,
+		version: request.version,
 		template_id: request.template_id
 	}
 
