816c73d3305f3a730b75d3b6e76289430675f601
hyunjoon0208 <juatis96@kookmin.ac.kr>
2024-05-27 16:20:28 +0900

    Fix stt Request

--- src/service/mystic/src/app/router/stt/__init__.py
+++ src/service/mystic/src/app/router/stt/__init__.py
@@ -10,6 +10,7 @@ router = APIRouter(prefix="/stt", tags=["stt"])
 
 class STTRequest(BaseModel):
 	audio_data: Any
+	type: str = "audio/webm"
 
 class STTResponse(BaseModel):
 	data: Any
@@ -17,8 +18,9 @@ class STTResponse(BaseModel):
 @router.post("/", response_model=STTResponse)
 async def stt(request: STTRequest):
 	audio_data = base64.b64decode(request.audio_data.encode())
+	suffix = "." + request.type.split("/")[1]
 
-	with tempfile.NamedTemporaryFile(delete=False, suffix=".webm") as temp_file:
+	with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as temp_file:
 		with open(temp_file.name, "wb") as audio_file:
 			audio_file.write(audio_data)
 		with open(temp_file.name, "rb") as audio_file:
